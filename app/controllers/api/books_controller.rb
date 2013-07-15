24bf99febdfa8d36b6c6cff2e6b6cd22efdbaf76
aqingsao <vinci.zhang@gmail.com>
2013-07-15 15:13:39 +0800

    use render json to respond with json string

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,10 +1,13 @@
 class Api::BooksController < ApplicationController
 	skip_before_filter :authenticate_user! # we do not need devise authentication here
 
-	  def index
-	    @books = Book.all
-	    respond_to do |format|
-	      format.json { render json: @books }
-	    end
-  end
+	def index
+    @books = Book.all
+    render json: @books
+	end
+
+	def show
+    @book = Book.find(params[:id])
+    render json: @book
+	end
 end
