cb1ddd3aadd55d8e04721d386347ee6ffba105ee
aqingsao <vinci.zhang@gmail.com>
2013-07-16 16:49:01 +0800

    Install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Qianxue2::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Have to define default url for devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
