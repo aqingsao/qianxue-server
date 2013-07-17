fb7d243e56eec2580e5d4d0ea0604acea22008d2
aqingsao <vinci.zhang@gmail.com>
2013-07-17 16:10:33 +0800

    add 3 tests for plan

--- spec/model/plan_spec.rb
+++ spec/model/plan_spec.rb
@@ -3,6 +3,14 @@ require File.join(File.dirname(__FILE__), "../spec_helper")
 describe Plan, "" do
 	it	"return -60 when due date is 60 seconds ago" do
 		plan = FactoryGirl.create(:plan, due_date: Time.now.ago(60))
-		plan.remaining_date.should <= -60
+		plan.remaining_date.should be_within(1).of(-60)
+	end
+	it	"return 0 when due date is now" do
+		plan = FactoryGirl.create(:plan, :due_date => Time.now)
+		plan.remaining_date.should be_within(1).of(0)
+	end
+	it	"return 60 when due date is 1 day later" do
+		plan = FactoryGirl.create(:plan, :due_date => 1.day.from_now)
+		plan.remaining_date.should be_within(1).of(1.day.to_int)
 	end
 end
