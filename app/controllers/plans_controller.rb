59c87e1a8fa4fb1bb5d9321abe5c3727bc561c26
aqingsao <vinci.zhang@gmail.com>
2013-07-18 10:43:24 +0800

    could add a new plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,4 +4,10 @@ class PlansController < ApplicationController
 
 	def show
 	end
+
+	def create
+		book = Book.find(params[:plans][:bookId])
+		@plan = Plan.create(:book_id => book.id, :user_id=>current_user.id, :due_date=>params[:plans][:dueDate])
+		redirect_to plans_path
+	end
 end
---
+++ spec/factories/book.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :book do
+    title 'book title'
+    subTitle 'book sub title'
+  end
+end
+
