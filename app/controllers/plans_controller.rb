77b69c385fb3741d06168b630534238828c468dc
aqingsao <vinci.zhang@gmail.com>
2013-07-18 14:40:59 +0800

    Change nav name

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -6,7 +6,15 @@ class PlansController < ApplicationController
 	end
 
 	def create
-		book = Book.find(params[:plans][:bookId])
+		begin
+			book = Book.find(params[:plans][:bookId])
+		rescue
+			logger.warn "Cannot find book with id #{params[:plans][:bookId]}"
+			raise "Cannot find book with id #{params[:plans][:bookId]}"
+		end
+ 
+ 		raise "User already choosed book #{book.id} in plan" if current_user.books.include? book
+
 		@plan = Plan.create(:book_id => book.id, :user_id=>current_user.id, :due_date=>params[:plans][:dueDate])
 		redirect_to plans_path
 	end
--- spec/controller/plans_controller_spec.rb
+++ spec/controller/plans_controller_spec.rb
@@ -2,16 +2,17 @@ require File.join(File.dirname(__FILE__), "../spec_helper")
 
 describe PlansController do
 	include Devise::TestHelpers
+
 	before (:each) do
     @user = FactoryGirl.create(:user, :email => "1@test.com")
 		@book = FactoryGirl.create(:book)
 
-		sign_in @user
+		# sign_in @user
 	end
   describe "POST #create" do
     it "responds successfully with an HTTP 200 status code" do
-      # post "/plans", :plans[bookId]=> '', :plans[dueDate]=>Time.now
-      # expect(response).to be_success
+      post "/plans", :plans[bookId]=> '', :plans[dueDate]=>Time.now
+      expect(response).to be_success
       # expect(response.status).to eq(200)
     end
   end
