441d13d4c549dc66e00e2fdd0a28e6c7609bb508
aqingsao <vinci.zhang@gmail.com>
2013-07-20 18:27:38 +0800

    show notes count in notebook index page

--- app/controllers/api/note_books_controller.rb
+++ app/controllers/api/note_books_controller.rb
@@ -2,10 +2,10 @@ class Api::NoteBooksController < ApplicationController
 	skip_before_filter :authenticate_user! # we do not need devise authentication here
 
 	def index
-    render json: NoteBook.all
+    render json: NoteBook.all.as_json(:include=> [:notes=>{:only=>["id"]}])
 	end
 
 	def show
-    render json: NoteBook.find(params[:id])
+    render json: NoteBook.find(params[:id]).as_json(:include=> [:notes=>{:only=>["id"]}])
 	end
 end
