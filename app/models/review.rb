47cd229ea633af83faf387ab04649e7d19931ebc
aqingsao <vinci.zhang@gmail.com>
2013-08-01 15:54:17 +0800

    add new model: review

---
+++ app/models/review.rb
@@ -0,0 +1,14 @@
+class Review < ActiveRecord::Base
+  # attr_accessible :title, :body
+  belongs_to :user
+  belongs_to :note
+  STATUS = { too_easy: 0, easy: 1, medium: 2, difficult: 3, too_difficult: 4 }
+
+  def status
+    STATUS.key(read_attribute(:status))
+  end
+ 
+  def status=(s)
+    write_attribute(:status, STATUS[s.to_sym])
+  end
+end
